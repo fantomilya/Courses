3d65ea44b18b0c065c04dd88baf38d9f9986c4e9
fantomilya <fantomilya48@gmail.com>
2018-09-11 19:59:09 +0300

    les21

---
+++ Les21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Extensions;
+
+namespace Les21
+{
+    class Program
+    {
+        static void Main()
+        {
+            FileStream file = File.Open(@"E:\test.txt", FileMode.Open);
+
+            BinaryReader reader = new BinaryReader(file);
+
+            string s = reader.ReadString();
+            byte[] bytes = reader.ReadBytes(4);
+            long number = reader.ReadInt64();
+
+            reader.Close();
+
+            Console.WriteLine(number);
+            foreach (byte b in bytes)
+            {
+                Console.Write("[{0}]", b);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
